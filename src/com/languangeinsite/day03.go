0b6934327528f898e1f8dabf82ff696a0766c2cb
zgzmt <1403681268@qq.com>
2021-01-09 18:25:43 +0800

    day03

--- src/com/languangeinsite/day03.go
+++ src/com/languangeinsite/day03.go
@@ -16,6 +16,24 @@ func bitTreePreOrderTra(root *bitTree) {
 	bitTreePreOrderTra(root.lef)
 	bitTreePreOrderTra(root.right)
 }
+//非递归先序遍历二叉树
+func preOrderTra(root *bitTree){
+	if root ==nil {
+		return
+	}
+	p := root
+	var stack []*bitTree
+	for p!=nil || stack != nil {
+		for p != nil {
+			fmt.Print("%d",p.val)
+			stack = append(stack,p)
+			p = p.lef
+		}
+			p = stack[len(stack)-1].right
+			stack = stack[:len(stack)-1]
+
+	}
+}
 
 //递归中序遍历二叉树
 func bitTreeMidOrderTra(root *bitTree) {
@@ -27,6 +45,28 @@ func bitTreeMidOrderTra(root *bitTree) {
 	bitTreeMidOrderTra(root.right)
 }
 
+//中序遍历二叉树，非递归
+func midOrderTra(root *bitTree)[]int{
+	if root != nil {
+		return nil
+	}
+	p := root
+	reslut := make([]int,0)
+	stack := make([]*bitTree,0)
+	for p !=nil || len(stack)!=0 {
+		for p!=nil {
+			stack = append(stack,p)
+			p = p.lef
+		}
+			p = stack[len(stack)-1]
+			reslut = append(reslut,p.val)
+			stack = stack[:len(stack)-1]
+			p = p.right
+
+	}
+	return reslut
+}
+
 //递归后序遍历二叉树
 func bitTreePosOrderTra(root *bitTree) {
 	if root == nil {
@@ -35,4 +75,30 @@ func bitTreePosOrderTra(root *bitTree) {
 	bitTreePosOrderTra(root.lef)
 	bitTreePosOrderTra(root.right)
 	fmt.Printf("%d", root.val)
+}
+
+//非递归后续遍历二叉树
+func posordertra(root *bitTree)[]int {
+	if root == nil {
+		return nil
+	}
+	result := make([]int,0)
+	stack := make([]*bitTree,0)
+	p := root
+	var lastNode *bitTree
+	if p!=nil || len(stack)!=0{
+		for p != nil {
+			stack = append(stack,p)
+			p = p.lef
+		}
+		node := stack[len(stack)-1]
+		if node.right == nil || node == lastNode {
+			stack = stack[:len(stack)-1]
+			result = append(result,node.val)
+			lastNode = node
+		}else{
+			p = node.right
+		}
+	}
+	return result
 }
--- src/main.go
+++ src/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	//"com.languangeinsite"
+	//"com/languangeinsite"
+	//"fmt"
+	"fmt"
+)
+type bitTree struct {
+	val int
+	lef ,rright *bitTree
+}
+var root * bitTree
+func init(){
+	var p *bitTree
+	p = &bitTree{
+		val:    0,
+		lef:    nil,
+		rright: nil,
+	}
+	for i := 1 ; i< 5 ; i++ {
+		p.lef = &bitTree{
+			val:    i,
+			lef:    nil,
+			rright: nil,
+		}
+		p.rright = &bitTree{
+			val:    i+10,
+			lef:    nil,
+			rright: nil,
+		}
+		p = p.lef
+	}
+	root = p
+}
 
 func main() {
-	fmt.Println("Hello world")
+	//fmt.Println("Hello world")
+	//languangeinsite.StrStr("Hello world","Hello")
+	fmt.Printf("root的类型%T\n",root)
 }
