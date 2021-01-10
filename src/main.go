ba039d8fe47c990677b20b045a2e2d1abf23876b
zgzmt <1403681268@qq.com>
2021-01-10 10:39:40 +0800

    day04

--- src/main.go
+++ src/main.go
@@ -6,36 +6,47 @@ import (
 	//"fmt"
 	"fmt"
 )
-type bitTree struct {
+type bitTreeNode struct {
 	val int
-	lef ,rright *bitTree
+	lef ,rright *bitTreeNode
 }
-var root * bitTree
+var root *bitTreeNode
 func init(){
-	var p *bitTree
-	p = &bitTree{
+	var p *bitTreeNode
+	p = &bitTreeNode{
 		val:    0,
 		lef:    nil,
 		rright: nil,
 	}
+	had := p
 	for i := 1 ; i< 5 ; i++ {
-		p.lef = &bitTree{
+		p.lef = &bitTreeNode{
 			val:    i,
 			lef:    nil,
 			rright: nil,
 		}
-		p.rright = &bitTree{
+		p.rright = &bitTreeNode{
 			val:    i+10,
 			lef:    nil,
 			rright: nil,
 		}
 		p = p.lef
 	}
-	root = p
+	fmt.Printf("head类型：%T\n",had)
+	fmt.Printf("headd地址：%p\n",had)
+	fmt.Printf("栈内p类型：%T\n",p)
+	fmt.Printf("栈内p指针地址：%p\n",p)
+	root = had
 }
 
 func main() {
 	//fmt.Println("Hello world")
 	//languangeinsite.StrStr("Hello world","Hello")
-	fmt.Printf("root的类型%T\n",root)
+	fmt.Printf("root的类型:%T\n",root)
+	fmt.Printf("root的类型:%p\n",root)
+	var testarr []int
+	if testarr == nil {
+		fmt.Println("yes")
+	}
+
 }
